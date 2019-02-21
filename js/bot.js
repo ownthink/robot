13ef0f73513cbf662b4fae21ef6f5e9b0788b806
Yener <help@ownthink.com>
2019-02-21 11:08:16 +0800

    Update bot.js

--- js/bot.js
+++ js/bot.js
@@ -15,7 +15,7 @@ function action()
 	var args= {
 			type : "get",
 			url:"https://api.ownthink.com/bot",
-			data : {"token" : "openbot", "spoken" : text.val()},
+			data : {"appid" : "openbot", "spoken" : text.val()},
 			success : function(redata)
 			{
 				var result = $.parseJSON(redata).data.info.text;
